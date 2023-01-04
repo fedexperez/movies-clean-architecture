11fc1efd3f139678793242e14804c11c2b35132d
Federico Perez Morales <federicoperez61@gmail.com>
2023-01-04 08:22:54 -0500

    project created, entity created, errors folder created

---
+++ README.md
@@ -0,0 +1,19 @@
+# Movies App
+
+App which aims to consume external APIs where it extracts information and based on it we create our models and use them to display widgets and those widgets are displayed in a beautiful way we will use what is the patch View scroll.
+
+We'll use a custom card widget.
+We will sketch the whole process of a search to make it look as natural as possible.
+Also we use the BLoC as state management pattern, and FVM that helps with the need for consistent app builds by referencing the Flutter SDK version used on a per-project basis.
+
+## Getting Started
+
+This project is a starting point for a Flutter Clean Architecture applciation.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- [Online documentation](https://docs.flutter.dev)
+- [Movies API](https://www.themoviedb.org/)
+- [Fernando Herrera Udemy Course](https://www.udemy.com/course/flutter-ios-android-fernando-herrera/)
---
+++ lib/features/movies/domain/entities/movie.dart
@@ -0,0 +1,55 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final bool adult;
+  final String? backdropPath;
+  final List<int> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String? posterPath;
+  final String? releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+  final String? heroId;
+
+  const Movie({
+    required this.adult,
+    this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+    this.heroId,
+  });
+
+  @override
+  List<Object?> get props => [
+        adult,
+        backdropPath,
+        genreIds,
+        id,
+        originalLanguage,
+        originalTitle,
+        overview,
+        popularity,
+        posterPath,
+        releaseDate,
+        title,
+        video,
+        voteAverage,
+        voteCount,
+      ];
+}
