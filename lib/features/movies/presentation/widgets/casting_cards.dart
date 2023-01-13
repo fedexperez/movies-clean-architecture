c13532a5ad3c9ef95b75292024ca6820b6bf8af2
Federico Perez Morales <federicoperez61@gmail.com>
2023-01-13 11:15:37 -0500

    casting cards

--- lib/features/movies/presentation/widgets/casting_cards.dart
+++ lib/features/movies/presentation/widgets/casting_cards.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:clean_architecture_movies/features/movies/domain/entities/cast.dart';
-import 'package:clean_architecture_movies/features/movies/presentation/blocs/bloc/cast_bloc.dart';
+import 'package:clean_architecture_movies/features/movies/presentation/blocs/cast/cast_bloc.dart';
 import 'package:clean_architecture_movies/injection_container.dart';
 
 class CastingCards extends StatelessWidget {
