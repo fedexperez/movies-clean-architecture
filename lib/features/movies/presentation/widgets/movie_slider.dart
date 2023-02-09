91ec400b61c82fbc5ec3ec8696494f4e71f82565
Federico Perez Morales <federicoperez61@gmail.com>
2023-02-09 16:34:50 -0500

    movie slider call function fixed

--- lib/features/movies/presentation/widgets/movie_slider.dart
+++ lib/features/movies/presentation/widgets/movie_slider.dart
@@ -24,12 +24,28 @@ class _MovieSliderScreenState extends State<MovieSliderScreen> {
   @override
   void initState() {
     super.initState();
+    bool canCall = true;
+    double maxScrollPixels = 0;
 
-    scrollController.addListener(() {
-      if (scrollController.position.pixels >=
-          scrollController.position.maxScrollExtent - 50) {
-        widget.onNextPage();
-        Future.delayed(const Duration(seconds: 2));
+    scrollController.addListener(() async {
+      if ((scrollController.position.maxScrollExtent > maxScrollPixels) &&
+          (canCall == false)) {
+        canCall = true;
+      }
+      print('el max scroll ${scrollController.position.maxScrollExtent}');
+      if (canCall == false) {
+        return;
+      }
+      if ((scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent - 500)) {
+        if (canCall) {
+          canCall = false;
+          await widget.onNextPage();
+          print('se llamó');
+        }
+        maxScrollPixels = scrollController.position.maxScrollExtent;
+        print('la VARIABLE $maxScrollPixels');
+        print(canCall);
       }
     });
   }
